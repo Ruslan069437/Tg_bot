f8bf41e9b73f01ed5c5b0cfbc46ca395f0777aed
Ahmedov_re <r52066523@gmail.com>
2025-03-31 14:44:19 +0200

    init

---
+++ models.py
@@ -0,0 +1,13 @@
+import sqlalchemy
+from sqlalchemy import Column, Integer, DECIMAL, String
+from sqlalchemy.orm import declarative_base
+
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+
+    user_id = Column(Integer, primary_key=True)
+    user_name = Column(String, nullable=False)
+    balance = Column(DECIMAL, nullable=False)
